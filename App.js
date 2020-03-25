d8e3c20d409218c837067de6010a497ef4e88766
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-03-26 03:43:38 +0900

    made scroll feature

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import WelcomeScreen from "./screens/WelcomeScreen";
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yeah! Open up App.js to start working on your app!</Text>
+      <WelcomeScreen />
     </View>
   );
 }

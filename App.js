b37a9796cfde2d5e2711a23aa3d3f87e84f10e21
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-03-26 16:01:38 +0900

    added header

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions } from 'react-native';
+import { Header } from "react-native-elements"
 
-import WelcomeScreen from "./screens/WelcomeScreen";
+
+
+const SCREEN_WIDTH  = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <WelcomeScreen />
-    </View>
-  );
+	return (
+		<View>
+			<StatusBar barStyle="light-content" hidden={false} translucent={true}/>
+			<Header
+				placement="left"
+				leftComponent={{ icon: 'menu', color: 'lightgrey' }}
+				centerComponent={{ text: 'MY IOS APP', style: { color: 'lightgrey' } }}
+				rightComponent={{ icon: 'home', color: 'lightgrey' }}
+				containerStyle={{ backgroundColor:"black" }}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });

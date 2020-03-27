6d99bec31e9c4ed4a388889b299e97ae74845417
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-03-28 02:19:57 +0900

    added simple drawer menu by https://www.youtube.com/watch?v=7uhJN4kVS6g

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Dimensions } from 'react-native';
-import { Header } from "react-native-elements"
-
+import { Header } from "react-native-elements";
+import { createDrawerNavigator } from "react-navigation";
+import HomeScreen from "./screens/HomeScreen";
+import SettingsScreen from "./screens/SettingsScreen";
 
 
 const SCREEN_WIDTH  = Dimensions.get("window").width;
@@ -10,18 +12,29 @@ const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 export default function App() {
 	return (
-		<View>
-			<StatusBar barStyle="light-content" hidden={false} translucent={true}/>
-			<Header
-				placement="left"
-				leftComponent={{ icon: 'menu', color: 'lightgrey' }}
-				centerComponent={{ text: 'MY IOS APP', style: { color: 'lightgrey' } }}
-				rightComponent={{ icon: 'home', color: 'lightgrey' }}
-				containerStyle={{ backgroundColor:"black" }}
-			/>
-		</View>
+		/*
+		 *<View>
+		 *   <StatusBar barStyle="light-content" hidden={false} translucent={true}/>
+		 *   <Header
+		 *      placement="left"
+		 *      leftComponent={{ icon: 'menu', color: 'lightgrey' }}
+		 *      centerComponent={{ text: 'MY IOS APP', style: { color: 'lightgrey' } }}
+		 *      rightComponent={{ icon: 'home', color: 'lightgrey' }}
+		 *      containerStyle={{ backgroundColor:"black" }}
+		 *      onPress={ ()=>this.props.navigation.navigate("DrawerOpen") }
+		 *   />
+		 *</View>
+		 */
+		<MenuBar />
 	);
 }
 
+
 const styles = StyleSheet.create({
 });
+
+
+const MenuBar = createDrawerNavigator({
+	Home:HomeScreen,
+	Settings:SettingsScreen
+})
